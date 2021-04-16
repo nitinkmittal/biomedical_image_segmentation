b34f7a71f936508c04be609634117ea41c0e1007
nitinkumarmittal <nitinmittal243@gmail.com>
2021-04-16 12:28:50 -0400

    Added original samples and masks

---
+++ src/biomedical_image_segmentation/data/validator.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple, Union
+
+import numpy as np
+from scipy import ndimage
+
+
+def validate(
+    dataset: List[Tuple[np.ndarray, np.ndarray]],
+    shape: Tuple[int, int] = None,
+    check_for_duplicates: bool = False,
+):
+    """Validate data."""
+    if shape is None:
+        shape = dataset[0][0].shape
+
+    for img, mask in dataset:
+
+        assert img.shape == shape
+        assert mask.shape == shape
+        assert np.allclose(np.unique(mask), np.array([0, 255]))
+
+    if check_for_duplicates:
+        assert len(np.unique([img for img, _ in dataset], axis=0)) == len(
+            dataset
+        )
+
+
